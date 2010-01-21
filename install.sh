f60c5b4f05ee58c4ac053144c4bf930b805bf883
Sedrik <sir.sedrik@gmail.com>
2010-01-21 11:11:34 +0100

    added a description and todo notes

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+#
+# A small install script for installing my configuration
+# TODO: Add a check to see if the file exists and diff them in that case
+# TODO: Add questions on which configuration files to install.
+#
+
 ln vimrc ../.vimrc
 ln rtorrent.rc ../.rtorrent.rc
