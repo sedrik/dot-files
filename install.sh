bfd01d839a2e97ffc7c559dea8d118559e876d1e
Sedrik <sir.sedrik@gmail.com>
2010-01-21 11:50:34 +0100

    added gitconfig

--- install.sh
+++ install.sh
@@ -10,12 +10,19 @@ VIMRC=~/.vimrc
 if [ $(ls $VIMRC) = 0 ]; then #file exists
     ln vimrc $VIMRC
 else
-    echo "The file ~/.vimrc exists, will not install vimrc"
+    echo "The file $VIMRC exists, will not install vimrc"
 fi
 
 RTORRENTRC=~/.rtorrent.rc
 if [ $(ls $RTORRENTRC) = 0 ]; then #file exists
     ln rtorrent.rc $RTORRENTRC
 else
-    echo "The file ~/.vimrc exists, will not install vimrc"
+    echo "The file $RTORRENTRC exists, will not install vimrc"
+fi
+
+GITCONFIG=~/.gitconfig
+if [ $(ls $GITCONFIG) = 0 ]; then #file exists
+    ln rtorrent.rc $GITCONFIG
+else
+    echo "The file $GITCONFIG exists, will not install vimrc"
 fi
