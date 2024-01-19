88dcddef565f156b29fa60e47a834b61bca21d57
Fredrik Park <sir.sedrik@gmail.com>
2024-01-19 09:45:32 +0100

    Setup FiraCodeNerd font and starship prompt

--- distro-setup/ubuntu.sh
+++ distro-setup/ubuntu.sh
@@ -14,6 +14,16 @@ sudo apt install nala
 echo "Update apt mirrors"
 sudo nala fetch
 
+echo "Setup git"
+sudo nala install git
+
+echo "Setup FiraCode Nerd font"
+git clone --depth=1 https://github.com/ryanoasis/nerd-fonts.git ~/.nerd-fonts
+pushd ~/.nerd-fonts
+./install.sh FiraCode
+popd
+rm -rf ~/.nerd-fonts
+
 echo "Setup regolith-desktop"
 wget -qO - https://regolith-desktop.org/regolith.key | gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
 echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
@@ -25,6 +35,9 @@ echo "Setup Alacritty"
 sudo nala install alacritty
 echo "Please select alacritty as the default terminal"
 sudo update-alternatives --config x-terminal-emulator
+mkdir -p ~/.config/alacritty/
+cp /usr/share/doc/alacritty/examples/alacritty.yml ~/.config/alacritty/
+# TODO Setup The fonts
 
 echo "Setup firefox-dev (to avoid the snap version)"
 sudo nala purge firefox
@@ -35,9 +48,6 @@ echo "Setup fish"
 sudo add-apt-repository ppa:fish-shell/release-3
 sudo nala install fish
 sudo chsh -s /usr/bin/fish sedrik
-
-echo "Setup git"
-sudo nala install git
 
 echo "Setup google-drive-ocamlfuse (to sync keepassxc files)"
 sudo add-apt-repository ppa:alessandro-strada/ppa
@@ -69,6 +79,11 @@ sudo nala install shellcheck
 
 echo "Setup unrar"
 sudo nala install unrar
+
+echo "Setup startship prompt"
+sudo nala install curl
+curl -sS https://starship.rs/install.sh | sh
+echo "starship init fish | source" >> ~/.config/fish/config.fish
 
 echo "Do a upgrade"
 sudo nala upgrade
