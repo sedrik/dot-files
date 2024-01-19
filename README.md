a3799493312d73270427c44cf5e7af4e89133de0
Fredrik Park <sir.sedrik@gmail.com>
2024-01-19 08:09:20 +0100

    Add basic ubuntu setup

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dotfiles
 Home sweet home
+
+## Setup distribution
+
+Distribution setup is based on the donothing pattern https://blog.danslimmon.com/2019/07/15/do-nothing-scripting-the-key-to-gradual-automation/
+
+### Ubuntu
+
+```bash
+./distro-setup/ubuntu.sh
+```
+
+## Install dotfiles
+
+```bash
+./install
+```
+
+## Thanks
+
+This repository was insipired by https://github.com/jessarcher/dotfiles
--- distro-setup/ubuntu.sh
+++ distro-setup/ubuntu.sh
@@ -6,6 +6,65 @@ press_enter() {
   read -r -p "Press enter to continue"
 }
 
-echo "Pull latest development"
-echo "> git checkout development; git pull"
-press_enter
+echo "Install nala (improved package manager ui)"
+sudo apt install nala
+
+echo "Update apt mirrors"
+sudo nala fetch
+
+echo "Setup regolith-desktop"
+wget -qO - https://regolith-desktop.org/regolith.key | gpg --dearmor | sudo tee /usr/share/keyrings/regolith-archive-keyring.gpg > /dev/null
+echo deb "[arch=amd64 signed-by=/usr/share/keyrings/regolith-archive-keyring.gpg] \
+https://regolith-desktop.org/release-3_0-ubuntu-lunar-amd64 lunar main" | sudo tee /etc/apt/sources.list.d/regolith.list
+sudo nala update
+sudo nala install regolith-desktop regolith-session-flashback regolith-look-nord
+
+echo "Setup Alacritty"
+sudo nala install alacritty
+echo "Please select alacritty as the default terminal"
+sudo update-alternatives --config x-terminal-emulator
+
+echo "Setup firefox-dev (to avoid the snap version)"
+sudo nala purge firefox
+sudo add-apt-repository ppa:mozillateam/firefox-next
+sudo nala install firefox-dev
+
+echo "Setup fish"
+sudo add-apt-repository ppa:fish-shell/release-3
+sudo nala install fish
+
+echo "Setup git"
+sudo nala install git
+
+echo "Setup google-drive-ocamlfuse (to sync keepassxc files)"
+sudo add-apt-repository ppa:alessandro-strada/ppa
+sudo nala install google-drive-ocamlfuse
+
+echo "Setup htop"
+sudo nala install htop
+
+echo "Setup jq"
+sudo nala install jq
+
+echo "Setup keepassxc"
+sudo add-apt-repository ppa:phoerious/keepassxc
+sudo nala install keepassxc
+
+echo "Setup lm-sensors"
+sudo nala install lm-sensors
+
+echo "Setup neovim (dev version)"
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo nala install neovim
+
+echo "Setup ripgrep"
+sudo nala install ripgrep
+
+echo "Setup shellcheck"
+sudo nala install shellcheck
+
+echo "Setup unrar"
+sudo nala install unrar
+
+echo "Do a upgrade"
+sudo nala upgrade
