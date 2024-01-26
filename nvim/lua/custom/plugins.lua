8109a148a4ef30cf7b766d665f182df69aa6b096
Fredrik Park <sir.sedrik@gmail.com>
2024-01-26 21:44:17 +0100

    lsp

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -1,5 +1,25 @@
 local plugins = {
   { "vimwiki/vimwiki" , lazy = false },
+
+  -- In order to modify the `lspconfig` configuration:
+{
+  "neovim/nvim-lspconfig",
+   config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+   end,
+},
+   {
+   "williamboman/mason.nvim",
+   opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua"
+      },
+    },
+  }
 }
 
 return plugins
