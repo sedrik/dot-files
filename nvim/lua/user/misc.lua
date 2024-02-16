eb50691568e72f777815acc963d5386d7a132fd8
Fredrik Park <sir.sedrik@gmail.com>
2024-02-16 15:31:20 +0100

    remove some trailing spaces

--- nvim/lua/user/misc.lua
+++ nvim/lua/user/misc.lua
@@ -1,6 +1,6 @@
--- Use internal formatting for bindings like gq. 
- vim.api.nvim_create_autocmd('LspAttach', { 
-   callback = function(args) 
-     vim.bo[args.buf].formatexpr = nil 
-   end, 
+-- Use internal formatting for bindings like gq
+ vim.api.nvim_create_autocmd('LspAttach', {
+   callback = function(args)
+     vim.bo[args.buf].formatexpr = nil
+  end,
  })
