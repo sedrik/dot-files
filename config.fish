fad8c5626f71affe12c43fbee0e018f872eccc54
Fredrik Park <sir.sedrik@gmail.com>
2024-09-25 08:10:04 +0200

    Updates

--- config.fish
+++ config.fish
@@ -10,10 +10,6 @@ if status is-interactive
     # sleep 1 & xmodmap ~/.Xmodmap > /dev/null
   starship init fish | source
 
-  if test ! -f ~/myGoogleDrive/Passwords.kdbx
-      google-drive-ocamlfuse ~/myGoogleDrive &
-  end
-
   nvm use lts
 
   zoxide init --cmd cd fish | source
